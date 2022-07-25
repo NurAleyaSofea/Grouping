1de53c13a1880fe0cafcfd0c9b8ccb8c801fbf45
NurAleyaSofeaAbdKarim <103669662+NurAleyaSofea@users.noreply.github.com>
2022-07-26 00:31:53 +0800

    Create Style.js

---
+++ Style.js
@@ -0,0 +1,27 @@
+// Create a Map
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+
+  const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
